4f7cfcf93cf8887c558e125c7208a9e2db076634
nirjankhadka <nirjan.khadka@gfinity.net>
2022-02-19 12:10:40 +0545

    refactor: add panelbear, favicons and downgrade node version

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,10 +1,29 @@
 import "../styles/normalize.css";
 import "../styles/global.css";
 import { DefaultSeo } from "next-seo";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        id="panelbear-js"
+        src="https://cdn.panelbear.com/analytics.js?site=ByU4h80Ugbv"
+        async={true}
+      />
+      <Script
+        id="panelbear-init"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.panelbear =
+          window.panelbear ||
+          function () {
+            (window.panelbear.q = window.panelbear.q || []).push(arguments);
+          };
+        panelbear('config', { site: 'ByU4h80Ugbv' });
+  `,
+        }}
+      />
       <DefaultSeo
         openGraph={{
           type: "website",
@@ -24,6 +43,36 @@ export default function MyApp({ Component, pageProps }) {
           site: "@nirjan_dev",
           cardType: "summary_large_image",
         }}
+        additionalLinkTags={[
+          {
+            rel: "apple-touch-icon",
+            href: "/apple-touch-icon.png",
+            sizes: "180x180",
+          },
+          {
+            rel: "icon",
+            href: "/favicon-32x32.png",
+            type: "image/png",
+            sizes: "32x32",
+          },
+          {
+            rel: "icon",
+            href: "/favicon-16x16.png",
+            type: "image/png",
+            sizes: "16x16",
+          },
+
+          {
+            rel: "manifest",
+            href: "/site.webmanifest",
+          },
+
+          {
+            rel: "mask-icon",
+            href: "/safari-pinned-tab.svg",
+            color: "#845ec2",
+          },
+        ]}
       />
       <Component {...pageProps} />
     </>
