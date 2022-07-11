74e115b95790d5c34cee91756ac18f44fcf104b7
nirjankhadka <nirjan.khadka@gfinity.net>
2022-07-11 08:29:28 +0545

    fix(frontend): :bug: use any to fix sanity.server type compilation issue

--- apps/frontend/lib/sanity.server.ts
+++ apps/frontend/lib/sanity.server.ts
@@ -3,10 +3,10 @@ import { createClient } from "next-sanity";
 import { config } from "./config";
 
 // Set up the client for fetching data in the getProps page functions
-export const sanityClient = createClient(config);
+export const sanityClient: any = createClient(config);
 
 // Set up a preview client with serverless authentication for drafts
-export const previewClient = createClient({
+export const previewClient: any = createClient({
   ...config,
   useCdn: false,
   token: process.env.SANITY_API_TOKEN,
