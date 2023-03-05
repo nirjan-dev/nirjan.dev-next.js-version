6b595c3ca559eee46045ffb359c242c8be2fa8c3
nirjankhadka <nirjan.khadka@gfinity.net>
2023-03-05 10:02:24 +0545

    feat: add clarity tracking script

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -40,6 +40,18 @@ export default function MyApp({ Component, pageProps }) {
         }}
       />
       <GoogleAnalytics trackPageViews />
+      <Script
+        id="clarity-script"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "g3oj3ab384");
+        `,
+        }}
+      />
       <DefaultSeo
         openGraph={{
           type: "website",
